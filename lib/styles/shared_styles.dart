d062c06839ff3c8d1145a52ee0541e113ca63d6f
montyr75 <montyr75@gmail.com>
2015-11-10 17:09:15 -0700

    added missing style import

--- lib/styles/shared_styles.dart
+++ lib/styles/shared_styles.dart
@@ -1,5 +1,6 @@
 @HtmlImport('shared_styles.html')
 library polymer_dart_starter_kit.lib.styles.shared_styles;
 
+import 'package:polymer_elements/paper_styles_classes.dart';
 import 'package:web_components/web_components.dart' show HtmlImport;
 import 'package:polymer_interop/polymer_interop.dart';
